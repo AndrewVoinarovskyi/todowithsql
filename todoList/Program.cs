9699c16e5a8858741b788aabeb28a87b0164ae13
Andrii Voinarovskyi <49862256+AndrewVoinarovskyi@users.noreply.github.com>
2021-04-06 18:02:07 +0300

    Add files via upload

--- todoList/Program.cs
+++ todoList/Program.cs
@@ -2,30 +2,30 @@
 using System.Threading.Tasks;
 using Npgsql;
 
-
-class Program
+namespace TodoList
 {
-
-    public static void Main()
+    class Program
     {
-        var connString = "Host=127.0.0.1;Username=todolist;Password=andrii;Database=todolist";
 
-        using var conn = new NpgsqlConnection(connString);
-        conn.Open();
-
-        // Create("random title");
-        Delete(12);
-        // Delete(11);
-        // Delete(10);
-
-        // Update(2, "Update");
-
-        Read();
-        // Retrieve all todo item rows
-        void Read()
+        public static void Main()
         {
-            using (var cmd = new NpgsqlCommand("SELECT title, done FROM todolist", conn))
+            var connString = "Host=127.0.0.1;Username=todolist;Password=andrii;Database=todolist";
+
+            using var conn = new NpgsqlConnection(connString);
+            conn.Open();
+
+            // Create("random title");
+            Delete(12);
+            // Delete(11);
+            // Delete(10);
+
+            // Update(2, "Update");
+
+            Read();
+            // Retrieve all todo item rows
+            void Read()
             {
+                using (var cmd = new NpgsqlCommand("SELECT title, done FROM todolist", conn))
                 using (var reader = cmd.ExecuteReader())
                 while (reader.Read())
                 {
@@ -35,37 +35,40 @@ class Program
                     Console.WriteLine($"- [{doneFlag}] {title}");
                 }
             }
-        }
 
-        // Insert one todo item
-        void Create(string title)
-        {
-            using (var insertCmd = new NpgsqlCommand("INSERT INTO todolist (title, done) VALUES (@title, false)", conn))
+            // Insert one todo item
+            void Create(TodoItem item)
             {
-            insertCmd.Parameters.AddWithValue("title", title);
-            insertCmd.ExecuteNonQuery();
+                using (var insertCmd = new NpgsqlCommand("INSERT INTO todolist (title, done) VALUES (@title, false)", conn))
+                {
+                    insertCmd.Parameters.AddWithValue("title", item.Title);
+                    insertCmd.Parameters.AddWithValue("id", item.Id);
+                    insertCmd.Parameters.AddWithValue("description", item.Description ?? "");
+                    insertCmd.Parameters.AddWithValue("due_date", NpgsqlTypes.NpgsqlDbType.Date, (object)item.DueDate ?? DBNull.Value);
+                    insertCmd.ExecuteNonQuery();
+                }
             }
-        }
 
-        void Update(int id, string title)
-        {
-            using (var insertCmd = new NpgsqlCommand("UPDATE todolist SET title=@title, done=false WHERE id=@id", conn))
+            void Update(TodoItem item)
             {
-                insertCmd.Parameters.AddWithValue("title", title);
+                using (var insertCmd = new NpgsqlCommand("UPDATE todolist SET description=@description title=@title, done=@done, due_date=@due_date WHERE id=@id", conn))
+                {
+                    insertCmd.Parameters.AddWithValue("title", item.Title);
+                    insertCmd.Parameters.AddWithValue("id", item.Id);
+                    insertCmd.Parameters.AddWithValue("description", item.Description ?? "");
+                    insertCmd.Parameters.AddWithValue("due_date", NpgsqlTypes.NpgsqlDbType.Date, (object)item.DueDate ?? DBNull.Value);
+                    insertCmd.ExecuteNonQuery();
+                }
+            }
+
+            void Delete(int id)
+            {
+                using (var insertCmd = new NpgsqlCommand("DELETE FROM todolist WHERE id=@id", conn))
+                {
                 insertCmd.Parameters.AddWithValue("id", id);
                 insertCmd.ExecuteNonQuery();
-            }
-        }
-
-        void Delete(int id)
-        {
-            using (var insertCmd = new NpgsqlCommand("DELETE FROM todolist WHERE id=@id", conn))
-            {
-            insertCmd.Parameters.AddWithValue("id", id);
-            insertCmd.ExecuteNonQuery();
+                }
             }
         }
     }
-
-    
 }
---
+++ todoList/TodoItem.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TodoList
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool Done { get; set; }
+    }
+}
