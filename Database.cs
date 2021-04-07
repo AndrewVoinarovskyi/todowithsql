76294c4610b71552a5e539f07da374215a664c85
AndrewVoinarovskyi <avoinarovskiy@gmail.com>
2021-04-07 10:17:43 +0300

    Realizad CRUD

---
+++ Database.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace TodoList
+{
+    class Database
+    {
+        NpgsqlConnection conn;
+        List<TodoItem> todoList = new List<TodoItem>();
+        public Database(string connString)
+        {
+            this.conn = new NpgsqlConnection(connString);
+            this.conn.Open();
+        }
+
+        public List<TodoItem> Read()
+        {
+            using (var cmd = new NpgsqlCommand("SELECT id, title, done, description, due_date FROM todolist", conn))
+            using (var reader = cmd.ExecuteReader())
+                while (reader.Read())
+                {
+                    TodoItem item = new TodoItem()
+                    {
+                        Id = reader.GetInt32(0),
+                        Title = reader.GetString(1),
+                        Done = reader.GetBoolean(2),
+                        Description = reader.IsDBNull(3) ? "" : reader.GetString(3),
+                    };
+                    if (!reader.IsDBNull(4))
+                    {
+                        item.DueDate = reader.GetDateTime(4);
+                    };
+
+                    char doneFlag = item.Done ? 'x' : ' ';
+                    Console.WriteLine($"- [{doneFlag}]\t{item.Id}\t{item.Title}\n" +
+                                        $"\t{item.Description}");
+                    todoList.Add(item);
+                }
+            return todoList;
+        }
+
+        public void Create(TodoItem item)
+        {
+            using (var insertCmd = new NpgsqlCommand("INSERT INTO todolist (title, done) VALUES (@title, false)", conn))
+            {
+                insertCmd.Parameters.AddWithValue("id", item.Id);
+
+                insertCmd.Parameters.AddWithValue("title", item.Title);
+                insertCmd.Parameters.AddWithValue("description", item.Description ?? "");
+                insertCmd.Parameters.AddWithValue("due_date", NpgsqlTypes.NpgsqlDbType.Date, (object)item.DueDate ?? DBNull.Value);
+                insertCmd.Parameters.AddWithValue("done", item.Done);
+                insertCmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(TodoItem item)
+        {
+            using (var insertCmd = new NpgsqlCommand("UPDATE todolist SET description=@description title=@title, done=@done, due_date=@due_date WHERE id=@id", conn))
+            {
+                insertCmd.Parameters.AddWithValue("id", item.Id);
+
+                insertCmd.Parameters.AddWithValue("title", item.Title);
+                insertCmd.Parameters.AddWithValue("description", item.Description ?? "");
+                insertCmd.Parameters.AddWithValue("due_date", NpgsqlTypes.NpgsqlDbType.Date, (object)item.DueDate ?? DBNull.Value);
+                insertCmd.Parameters.AddWithValue("done", item.Done);
+                insertCmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var insertCmd = new NpgsqlCommand("DELETE FROM todolist WHERE id=@id", conn))
+            {
+                insertCmd.Parameters.AddWithValue("id", id);
+                insertCmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Npgsql;
 
@@ -9,66 +10,9 @@ namespace TodoList
 
         public static void Main()
         {
-            var connString = "Host=127.0.0.1;Username=todolist;Password=andrii;Database=todolist";
-
-            using var conn = new NpgsqlConnection(connString);
-            conn.Open();
-
-            // Create("random title");
-            Delete(12);
-            // Delete(11);
-            // Delete(10);
-
-            // Update(2, "Update");
-
-            Read();
-            // Retrieve all todo item rows
-            void Read()
-            {
-                using (var cmd = new NpgsqlCommand("SELECT title, done FROM todolist", conn))
-                using (var reader = cmd.ExecuteReader())
-                while (reader.Read())
-                {
-                    var title = reader.GetString(0);
-                    var done = reader.GetBoolean(1);
-                    char doneFlag = done ? 'x' : ' ';
-                    Console.WriteLine($"- [{doneFlag}] {title}");
-                }
-            }
-
-            // Insert one todo item
-            void Create(TodoItem item)
-            {
-                using (var insertCmd = new NpgsqlCommand("INSERT INTO todolist (title, done) VALUES (@title, false)", conn))
-                {
-                    insertCmd.Parameters.AddWithValue("title", item.Title);
-                    insertCmd.Parameters.AddWithValue("id", item.Id);
-                    insertCmd.Parameters.AddWithValue("description", item.Description ?? "");
-                    insertCmd.Parameters.AddWithValue("due_date", NpgsqlTypes.NpgsqlDbType.Date, (object)item.DueDate ?? DBNull.Value);
-                    insertCmd.ExecuteNonQuery();
-                }
-            }
-
-            void Update(TodoItem item)
-            {
-                using (var insertCmd = new NpgsqlCommand("UPDATE todolist SET description=@description title=@title, done=@done, due_date=@due_date WHERE id=@id", conn))
-                {
-                    insertCmd.Parameters.AddWithValue("title", item.Title);
-                    insertCmd.Parameters.AddWithValue("id", item.Id);
-                    insertCmd.Parameters.AddWithValue("description", item.Description ?? "");
-                    insertCmd.Parameters.AddWithValue("due_date", NpgsqlTypes.NpgsqlDbType.Date, (object)item.DueDate ?? DBNull.Value);
-                    insertCmd.ExecuteNonQuery();
-                }
-            }
-
-            void Delete(int id)
-            {
-                using (var insertCmd = new NpgsqlCommand("DELETE FROM todolist WHERE id=@id", conn))
-                {
-                insertCmd.Parameters.AddWithValue("id", id);
-                insertCmd.ExecuteNonQuery();
-                }
-            }
+            Database database = new Database("Host=127.0.0.1;Username=todolist;Password=andrii;Database=todolist");
+            List<TodoItem> todoList = database.Read();
+            Console.WriteLine(todoList);
         }
     }
 }
